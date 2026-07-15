3112bd6a84ce5b1d93a526fc5888d9fb0900fe07
archanakandavel07 <archanakandavel07@gmail.com>
2026-07-15 09:24:10 +0000

    added2

---
+++ calci2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+        int a,b;
+        cout<<"Enter 2 no:";
+        cin>>a>>b;
+        char ch;
+        cout<<"Enter operator:";
+        cin>>ch;
+        switch(ch)
+        {
+                case '*':
+                        cout<<"Result:"<<(a*b);
+                        break;
+                case '/':
+                        cout<<"Result:"<<(a/b);
+                        break;
+                default:
+                        cout<<"Invalid";
+        }
+}
